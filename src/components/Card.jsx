cc91a3d709ef94c5af7d1f163137002d75417ee7
Jahnavi <130915370+Jahnavi-57@users.noreply.github.com>
2024-05-03 11:58:22 +0530

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+
+export default function Card(props)
+{
+    let badgeText
+            if(props.openSpots===0)
+            {
+                badgeText="SOLD OUT"
+            }else if(props.location==="Online")
+            {
+                badgeText="ONLINE"
+            }
+    return(
+        <div className="card">
+
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={props.coverImg} className="card--image" alt="Image"/>
+            <div className="card--stats">
+                <img src="./src/images/star.png" className="card--star" alt="start icon"/>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>
+            </div>
+            <h2>{props.title}</h2>
+            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
+        </div>
+    )
+
+}
