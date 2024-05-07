40c83662e458a8f2d260481dff449d4f69c9841c
Jahnavi <130915370+Jahnavi-57@users.noreply.github.com>
2024-05-07 14:17:18 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   base:'/AirBnb-clone-using-reactjs/',
-  plugins: [react()],
+  plugins: [reactRefresh()],
 })
