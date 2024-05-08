b037238757840bedaa7c22a8a74b4bd791d23625
Jahnavi <130915370+Jahnavi-57@users.noreply.github.com>
2024-05-08 11:09:24 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar()
 {
     return(
         <nav>
-            <img src="./src/images/airbnb-logo.png" className="nav--logo"></img>
+            <img src="images/airbnb-logo.png" className="nav--logo"></img>
         </nav>
     )
 }
